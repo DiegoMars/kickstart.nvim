81d0fb8ee308458850eb4797b522f6244b27871a
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-05-09 09:52:56 -0700

    Fixed the markdownPreview stuff (Baller LMAO)

--- lua/custom/plugins/markdownPreview.lua
+++ lua/custom/plugins/markdownPreview.lua
@@ -5,12 +5,26 @@
 --:call mkdp#util#install()
 --And it worked, idk if would work now that I have a different
 --install method tho, but we'll see
+--
+-- return {
+--   "iamcco/markdown-preview.nvim",
+--   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--   build = "cd app && yarn install",
+--   init = function()
+--     vim.g.mkdp_filetypes = { "markdown" }
+--   end,
+--   ft = { "markdown" },
+-- }
+--
+-- Yea that did not work LMAO
+-- Here is what worked then, and remember the whole
+-- :call mkdp#util#install()
+-- Stuff when using this
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
   end,
-  ft = { "markdown" },
 }
