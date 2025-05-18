93a88a784e2d68f4103371a6db0efd74e5da004f
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-05-17 23:48:13 -0700

    Added markdown preview keymap

--- lua/custom/plugins/myremap.lua
+++ lua/custom/plugins/myremap.lua
@@ -20,19 +20,19 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
--- 
+--
 -- HTML rempas. These are run html sites with just keyboard shortcuts
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "html",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'html',
   callback = function()
     vim.keymap.set('n', '<Leader>r', function()
-      local file = vim.fn.expand('%:p')  -- full path of current file
+      local file = vim.fn.expand '%:p' -- full path of current file
       local open_cmd
       -- This only works when in windows
-      if vim.fn.has('win32') == 1 then
+      if vim.fn.has 'win32' == 1 then
         open_cmd = 'start "" "' .. file .. '"'
       else
-      -- This is for Ubuntu, but idk where else it could work 
+        -- This is for Ubuntu, but idk where else it could work
         open_cmd = 'xdg-open "' .. file .. '"'
       end
       os.execute(open_cmd)
@@ -42,28 +42,36 @@ vim.api.nvim_create_autocmd("FileType", {
 
 --
 -- Cpp remaps
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "cpp",
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'cpp',
   callback = function()
-    vim.keymap.set("n", "<leader>r", function()
-      local filepath = vim.fn.expand("%:p")      -- Gets the full path to the current file
-      local filedir = vim.fn.expand("%:p:h")     -- Gets the directory of the current file
-      local filename = vim.fn.expand("%:t:r")    -- Gets just the file name without extension
-      local is_windows = package.config:sub(1, 1) == "\\"
+    vim.keymap.set('n', '<leader>r', function()
+      local filepath = vim.fn.expand '%:p' -- Gets the full path to the current file
+      local filedir = vim.fn.expand '%:p:h' -- Gets the directory of the current file
+      local filename = vim.fn.expand '%:t:r' -- Gets just the file name without extension
+      local is_windows = package.config:sub(1, 1) == '\\'
       -- The above is kinda clever, it checks what the directory separator looks like
       -- For windows it is `\` and on linux it is `/`
 
-      local exe = is_windows
-        and (filedir .. "\\" .. filename .. ".exe") -- For windows, uses `\`
-                                                    -- and adds the .exe at the end
-        or (filedir .. "/" .. filename)             -- For linux, uses `/`
+      local exe = is_windows and (filedir .. '\\' .. filename .. '.exe') -- For windows, uses `\`
+        -- and adds the .exe at the end
+        or (filedir .. '/' .. filename) -- For linux, uses `/`
 
-      local compile_cmd = string.format("g++ \"%s\" -o \"%s\" && \"%s\"", filepath, exe, exe)
+      local compile_cmd = string.format('g++ "%s" -o "%s" && "%s"', filepath, exe, exe)
       -- This creates the cmd string depending on the settings applied
 
-      vim.cmd("split | terminal " .. compile_cmd) -- Runs the command in a horizontally
-                                                  -- split terminal
-    end, { buffer = true, desc = "[r]un cpp" })
+      vim.cmd('split | terminal ' .. compile_cmd) -- Runs the command in a horizontally
+      -- split terminal
+    end, { buffer = true, desc = '[r]un cpp' })
+  end,
+})
+
+--
+-- Markdown remaps
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.keymap.set('n', '<leader>r', ':MarkdownPreview<CR>', { buffer = true, desc = '[R]un Markdown' })
   end,
 })
 
