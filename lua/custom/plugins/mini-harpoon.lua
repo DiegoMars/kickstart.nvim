f4e018fece19666678a5049b331a02f692de4208
Diegos_Rad <131321814+DiegoMars@users.noreply.github.com>
2025-05-17 23:51:07 -0700

    Finished implemeting my mini-harpoon

---
+++ lua/custom/plugins/mini-harpoon.lua
@@ -0,0 +1,36 @@
+-- Fine, I'll do it myself
+return {
+  "DiegoMars/mini-harpoon",
+  config = function()
+    local harpoon = require("mini-harpoon")
+
+    -- Add buffer to the list of harpooned buffers
+    vim.keymap.set("n", "<leader>ha", harpoon.add, { desc = "[h]arpoon [a]dd" })
+
+    -- Moves to that buffer
+    vim.keymap.set("n", "<leader>1", function() harpoon.goto(1) end, { desc = "Buffer [1]" })
+    vim.keymap.set("n", "<leader>2", function() harpoon.goto(2) end, { desc = "Buffer [2]" })
+    vim.keymap.set("n", "<leader>3", function() harpoon.goto(3) end, { desc = "Buffer [3]" })
+    vim.keymap.set("n", "<leader>4", function() harpoon.goto(4) end, { desc = "Buffer [4]" })
+
+    -- Remove active buffer from the list if it is in it
+    vim.keymap.set("n", "<leader>hr", function()
+      local current = vim.api.nvim_buf_get_name(0)
+
+      for i, path in ipairs(harpoon.list) do
+        if path == current then
+          table.remove(harpoon.list, i)
+          print("Removed: " .. path)
+          harpoon.save()
+          return
+        end
+      end
+
+      print("Current file not in Harpoon list")
+    end, { desc = "Remove current file from Harpoon" })
+
+    -- Show the current list of files
+    vim.keymap.set("n", "<leader>he", harpoon.telescope, { desc = "[h]arpoon [e]xplore" })
+  end,
+  dependencies = { "nvim-telescope/telescope.nvim" },
+}
